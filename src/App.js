ff902376eaa6b7ab9b6b60d4b73f0375daf6d1b7
Zumotok <jonibujari@gmail.com>
2023-08-16 17:10:33 +0200

    list-filter-and-search

--- src/App.js
+++ src/App.js
@@ -1,83 +1,73 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("");
-  const [editingTodoId, setEditingTodoId] = useState(null);
-  const handleAddTodo = () => {
-    if (newTodo.trim() !== "") {
-      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
-      setNewTodo("");
-    }
+  const [list] = useState([
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eva",
+    "Frank",
+    "Grace",
+    "Helen",
+    "Isaac",
+    "Jack",
+  ]);
+  const [filterValues, setFilterValues] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleFilterChange = (event) => {
+    const options = event.target.options;
+    const values = Array.from(options)
+      .filter((option) => option.selected)
+      .map((option) => option.value);
+    setFilterValues(values);
   };
-  const handleToggle = (id) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
   };
-  const handleEdit = (id, newText) => {
-    const updatedTodos = todos.map((todo) =>
-      todo.id === id ? { ...todo, text: newText } : todo
-    );
-    setTodos(updatedTodos);
+
+  const filteredItems = () => {
+    return list
+      .filter((item) =>
+        filterValues.length === 0 ? item : filterValues.includes(item)
+      )
+      .filter((item) =>
+        input ? item.toLowerCase().includes(input.toLowerCase()) : item
+      );
   };
-  const handleDelete = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
-  };
+
+  const filteredList = filteredItems();
+
   return (
-    <div className="App">
-      <h1>Todo App</h1>
-      <div>
+    <div className="app-container">
+      <h1 className="app-title">Filter and Search List</h1>
+      <div className="filter-container">
+        <select
+          multiple
+          onChange={handleFilterChange}
+          className="filter-select"
+        >
+          {list.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
         <input
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Enter a new task"
+          value={input}
+          onChange={handleInput}
+          className="filter-input"
+          placeholder="Search"
         />
-        <button onClick={handleAddTodo}>Add</button>
       </div>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {!editingTodoId && (
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => handleToggle(todo.id)}
-              />
-            )}
-            {editingTodoId === todo.id ? (
-              <input
-                type="text"
-                value={todo.text}
-                onChange={(e) => handleEdit(todo.id, e.target.value)}
-                onBlur={() => setEditingTodoId(null)} // Done editing
-                autoFocus
-              />
-            ) : (
-              <span
-                style={{
-                  textDecoration: todo.completed ? "line-through" : "none",
-                }}
-              >
-                {todo.text}
-              </span>
-            )}
-            <button onClick={() => setEditingTodoId(todo.id)}>
-              {editingTodoId === todo.id
-                ? "Save"
-                : editingTodoId !== todo.id && !todo.completed
-                ? "Edit"
-                : null}
-            </button>
-            {editingTodoId !== todo.id && (
-              <button onClick={() => handleDelete(todo.id)}>Delete</button>
-            )}
+      <ul className="item-list">
+        {filteredList.map((item) => (
+          <li key={item} className="item">
+            {item}
           </li>
         ))}
       </ul>
